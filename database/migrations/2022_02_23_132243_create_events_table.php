6606129383510b8b1f1b0236058d3a493d639725
Gabriele Almeida <naogabe@gmail.com>
2022-02-23 10:30:07 -0300

    refactor: Criando a migration de eventos para o projeto final

--- database/migrations/2022_02_23_132243_create_events_table.php
+++ database/migrations/2022_02_23_132243_create_events_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddCategoryToProductsTable extends Migration
+class CreateEventsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,13 @@ class AddCategoryToProductsTable extends Migration
      */
     public function up()
     {
-        Schema::table('products', function (Blueprint $table) {
-            $table->string('category', 120);
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('title');
+            $table->text('description');
+            $table->string('city');
+            $table->boolean('private');
         });
     }
 
@@ -25,8 +30,6 @@ class AddCategoryToProductsTable extends Migration
      */
     public function down()
     {
-        Schema::table('products', function (Blueprint $table) {
-            $table->dropColumn('category');
-        });
+        Schema::dropIfExists('events');
     }
 }
