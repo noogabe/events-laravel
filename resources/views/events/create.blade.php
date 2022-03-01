1fdad13b39eacaf17f258c5ad0a309262d311e70
Gabriele Almeida <naogabe@gmail.com>
2022-03-01 17:04:55 -0300

    feat: Aprendendo como salvar imagens

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -6,25 +6,29 @@
 
 <div id="event-create-container" class="col-md-6 offset-md-3">
     <h1>Crie o seu evento</h1>
-    <form action="/events" method="POST">
+    <form action="/events" method="POST" enctype="multipart/form-data">
         @csrf
+        <div class="form-group">
+            <label for="image">Imagem do evento:</label> 
+            <input type="file" id="image" name="image" class="form-control-file">
+        </div>
         <div class="form-group">
             <label for="title">Evento:</label> 
             <input type="text" class="form-control" id="title" name="title" placeholder="Nome do evento">
         </div>
         <div class="form-group">
-            <label for="title">Cidade:</label> 
+            <label for="city">Cidade:</label> 
             <input type="text" class="form-control" id="city" name="city" placeholder="Local do evento">
         </div>
         <div class="form-group">
-            <label for="title">O evento é privado?</label> 
+            <label for="private">O evento é privado?</label> 
             <select name="private" id="private" class="form-control">
                 <option value="0">Não</option>
                 <option value="1">Sim</option>
             </select>
         </div>
         <div class="form-group">
-            <label for="title">Descrição:</label> 
+            <label for="description">Descrição:</label> 
             <textarea name="description" id="description" class="form-control" placeholder="O que vai acontecer no evento?"></textarea>
         </div>
         <input type="submit" class="btn btn-primary" value="Criar Evento">
