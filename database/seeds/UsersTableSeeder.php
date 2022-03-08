3e53d7b3c8fc4026a83f9e7dd8cbd17292422136
Gabriele Almeida <naogabe@gmail.com>
2022-03-08 14:38:27 -0300

    refactor: Criando um seeder, aprendendo a inserir uma instancia do Model User, depois aprendendo como utilizar factory para gerar vários registros de teste

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // $user = new User();
+        // $user->create([
+        //     'name' => 'Gabriele', 
+        //     'email' => 'gabriele@email.com.br', 
+        //     'password' => bcrypt('miau1234')
+        // ]);
+
+        factory(User::class, 30)->create();
+    }
+}
