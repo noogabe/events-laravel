66cf3d4ffd8d01cad67997b4b73ca7f2b140ee28
Gabriele Almeida <naogabe@gmail.com>
2022-03-24 09:54:54 -0300

    feat: Implementando seção de 'eventos que estou participando' na dashboard

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -114,7 +114,11 @@ class EventController extends Controller
 
         $events = $user->events;
 
-        return view('events.dashboard', ['events' => $events]);
+        $eventsAsParticipant = $user->eventsAsParticipant;
+
+        return view('events.dashboard', 
+            ['events' => $events, 'eventsAsParticipant' => $eventsAsParticipant]
+        );
     }
 
 
